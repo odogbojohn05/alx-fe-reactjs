73c287dc04bace48f6656343a76daf61216398f8
odogbojohn05 <ebubeodogbo@email.com>
2026-01-11 11:12:41 +0100

    Resolve merge conflicts

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -31,5 +31,17 @@ function App() {
     </>
   )
 }
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+
 
 export default App
+
