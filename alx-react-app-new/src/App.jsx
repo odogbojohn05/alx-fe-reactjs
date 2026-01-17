71dfc1fd8f09542a011c206c442f791bf09ebb41
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2026-01-17 20:01:29 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      <Header />
+      <UserProfile name="Ebube" age={25} bio="React developer from Benin City" />
       <MainContent />
       <Footer />
     </div>
