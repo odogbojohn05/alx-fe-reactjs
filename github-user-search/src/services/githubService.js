9ff8032376ddd8cf570ef5eb7f390fc7f9b2df96
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2026-01-31 18:50:26 +0100

    Refactor fetchUserData to advancedUserSearch

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-export async function fetchUserData(username) {
+export async function advancedUserSearch(username, location, minRepos) {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
+    let query = "";
+    if (username) query += `${username} in:login `;
+    if (location) query += `location:${location} `;
+    if (minRepos) query += `repos:>=${minRepos}`;
+
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+    return response.data.items; 
   } catch (error) {
     throw new Error("Looks like we cant find the user");
   }
