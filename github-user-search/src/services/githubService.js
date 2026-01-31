9d56b15c6d1a6547109b0a02339f34508e80b46a
ODOGBO JOHN ADEKUNLE <ebubeodogbo@gmail.com>
2026-01-31 18:34:25 +0100

    Simplify error message for user not found

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,9 +5,6 @@ export async function fetchUserData(username) {
     const response = await axios.get(`https://api.github.com/users/${username}`);
     return response.data;
   } catch (error) {
-
-    throw new Error(error.response?.status === 404 
-      ? "User not found" 
-      : "Error fetching user data");
+    throw new Error("Looks like we cant find the user");
   }
 }
